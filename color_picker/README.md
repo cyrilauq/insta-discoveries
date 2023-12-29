9d126471acc14c25e5bbaf7fe20929c580fca8bf
Auquier Cyril <c.auquier@student.helmo.be>
2023-12-29 20:00:21 +0100

    add some info inside the code and the readme

--- color_picker/README.md
+++ color_picker/README.md
@@ -3,3 +3,7 @@
 This project is about a color picker that use an image as a reference.
 
 Idea that comes from: https://www.instagram.com/p/C0RpZaCtmfy/
+
+## Project use
+
+- EyeDropper: The EyeDropper interface represents an instance of an eyedropper tool that can be opened and used by the user to select colors from the screen.(-- src: https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper)
--- color_picker/js/main.js
+++ color_picker/js/main.js
@@ -2,8 +2,8 @@ const button = document.getElementById("picker_btn")
 const pickerResult = document.getElementById("picker_result")
 
 const colorPicker = async () => {
-    const eyeDropper = new EyeDropper()
-    const { sRGBHex } = await eyeDropper.open()
+    const eyeDropper = new EyeDropper() // Returns a new EyeDropper instance.
+    const { sRGBHex } = await eyeDropper.open() // Returns a promise that resolves to an object that gives access to the selected color.
     pickerResult.innerText = sRGBHex
 }
 
